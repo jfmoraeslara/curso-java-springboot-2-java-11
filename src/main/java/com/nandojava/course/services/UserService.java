49ac1909b28d681cc0c396ce5fd49779841e321b
João <jfmoraeslara@gmail.com>
2022-10-07 23:33:02 -0300

    Exception handling - delete

--- src/main/java/com/nandojava/course/services/UserService.java
+++ src/main/java/com/nandojava/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.nandojava.course.entities.User1;
 import com.nandojava.course.repositories.UserRepository;
+import com.nandojava.course.services.exceptions.DatabaseException;
 import com.nandojava.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}		
 	}
 	
 	public User1 update(Long id, User1 obj) {
